4db7ac50ddfca60236c6319beb2413d7cb1c3c36
coralvanda <coralvanda@gmail.com>
2023-02-12 15:20:34 -0500

    Fix returned type

--- app/services/poke_services.py
+++ app/services/poke_services.py
@@ -12,18 +12,18 @@ from typing import List
 from app.services.unit_of_work import BaseUnitOfWork
 
 
-def get_single_pokemon(pokedex_number: int, unit_of_work: BaseUnitOfWork) -> str:
+def get_single_pokemon(pokedex_number: int, unit_of_work: BaseUnitOfWork) -> dict:
     with unit_of_work:
         target_pokemon = unit_of_work.pokemon.get_one(poke_id=pokedex_number)
 
     if not target_pokemon:
         abort(404)
 
-    return target_pokemon.to_json()
+    return target_pokemon.to_dict()
 
 
-def get_all_pokemon(unit_of_work: BaseUnitOfWork) -> List[str]:
+def get_all_pokemon(unit_of_work: BaseUnitOfWork) -> List[dict]:
     with unit_of_work:
         all_pokemon = unit_of_work.pokemon.get_all()
 
-    return [pokemon.to_json() for pokemon in all_pokemon]
+    return [pokemon.to_dict() for pokemon in all_pokemon]
